2b6af7e97c4334d2906b18c852ba34bff69c8ca1
rewqla <desares526@gmail.com>
2025-04-23 09:37:57 +0300

    Added mapbox refit client

---
+++ backend/ExternalApis/IMapboxApi.cs
@@ -0,0 +1,16 @@
+﻿using ExternalApis.Models;
+using Refit;
+
+namespace ExternalApis;
+
+public interface IMapboxApi
+{
+    [Get("/directions/v5/mapbox/{profile}/{coordinates}")]
+    Task<MapboxDirectionsResult> GetDirectionsAsync(
+        string profile,
+        string coordinates,
+        string steps,
+        string geometries,
+        string language,
+        string access_token);
+}
